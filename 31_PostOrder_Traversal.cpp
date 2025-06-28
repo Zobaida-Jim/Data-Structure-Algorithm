d16f08a06d536af9a5a69680d506225cf9592ed7
Zobaida Jim <z.jimcstucse@gmail.com>
2025-06-28 11:56:17 +0600

    Add files via upload

---
+++ 31_PostOrder_Traversal.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* buildTree(vector<int> nodes) // Build a tree and return the pointer of Root Node
+{
+    idx++;
+    if(nodes[idx] == -1)
+    {
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[idx]);
+    currNode->left = buildTree(nodes);//Left subtree
+    currNode->right = buildTree(nodes);//Right subtree
+
+    return currNode;
+}
+
+void postOrder(Node* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+
+    postOrder(root->left);//left Subtree
+    postOrder(root->right);//right Subtree
+    cout<<root->data<<" ";// root
+}
+
+int main()
+{
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, - 1};
+    Node* root = buildTree(nodes);
+
+    postOrder(root);
+    cout<<endl;
+
+    return 0;
+}
