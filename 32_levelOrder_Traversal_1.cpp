7ecd8316007bebf4ed422520c992b419af0fa14f
Zobaida Jim <z.jimcstucse@gmail.com>
2025-06-29 07:04:13 +0600

    Add files via upload

---
+++ 32_levelOrder_Traversal_1.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node *buildTree(vector<int> node)
+{
+    idx++;
+    if (node[idx] == -1)
+    {
+        return NULL;
+    }
+
+    Node *currNode = new Node(node[idx]);
+    currNode->left = buildTree(node);
+    currNode->right = buildTree(node);
+
+    return currNode;
+}
+
+void levelOrder(Node *root) // TC = O(n), SC = O(n)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+        cout << curr->data << " ";
+        if (curr->left != NULL)
+        {
+            q.push(curr->left);
+        }
+        if (curr->right != NULL)
+        {
+            q.push(curr->right);
+        }
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> node = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    Node *root = buildTree(node);
+
+    levelOrder(root);
+}
