1b9eda9e8735f23eed52be71d09e4bb81975d9de
Zobaida Jim <z.jimcstucse@gmail.com>
2025-06-30 09:09:32 +0600

    Add files via upload

---
+++ 35_countOfNodes_BT.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node *buildTree(vector<int> node)
+{
+    idx++;
+    if (node[idx] == -1)
+    {
+        return NULL;
+    }
+
+    Node *currNode = new Node(node[idx]);
+    currNode->left = buildTree(node);
+    currNode->right = buildTree(node);
+
+    return currNode;
+}
+
+int count_of_Nodes(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0; // number of nodes for empty tree
+    }
+    int leftc = count_of_Nodes(root->left);
+    int rightc = count_of_Nodes(root->right);
+
+    return (leftc + rightc + 1);
+}
+
+int main()
+{
+    vector<int> node = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    Node *root = buildTree(node);
+
+    cout << "Number of Nodes : " << count_of_Nodes(root) << endl;
+}
